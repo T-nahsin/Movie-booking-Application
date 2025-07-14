bd927f77eaa9418aa41e98e80c13708f870e9f5c
T-nahsin <nishant16405@gmail.com>
2025-07-14 15:07:10 +0530

    removed some directories

--- bookMovies/src/main/java/com/tnahsin/bookMovies/repository/ShowRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tnahsin.bookMovies.repository;
-
-import com.tnahsin.bookMovies.entity.Show;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ShowRepository extends MongoRepository<Show, ObjectId> {
-}
